1d8934df678bdd4aa0fcb75da74b07af860cbb3f
Gabriel S <gabriel.siket@gmail.com>
2022-07-19 08:15:01 +0000

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,42 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    //code block
+    return a + b;
+}
+let sum = addTwoNumbers(2, 3);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b;
+}
+let sum = addTwoNumbers(2, 3);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+// const addTwoNumbers2 = (a,b) => (a + b); also valid
+const addTwoNumbers2 = (a,b) => a + b;
+let sum2 = addTwoNumbers2(2, 3);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello!')
+
+const sayHello = () => console.log('hello!');
+sayHello()
+
 // Returning Multiple Lines
+
+const returnMutipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMutipleLines());
